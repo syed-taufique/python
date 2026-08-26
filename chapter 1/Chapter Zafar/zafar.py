8fa40c5d00d5f1d7c5a3ae67378cf3cbafdd9b9b
Taufique Ali <taufiqueali01@gmail.com>
2026-08-26 23:00:36 +0530

    Correct sum calculation and output in
    Fix calculation and output for sum of two numbers.

--- chapter 1/Chapter Zafar/zafar.py
+++ chapter 1/Chapter Zafar/zafar.py
@@ -2,3 +2,7 @@ a =int(input("Enter a number: "))
 b=int(input("Enter another number: "))
 sum = a *b
 print("The product of", a, "and", b, "is:", sum)
+a =int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+sum = a +b
+print("The product of", a, "and", b, "is:", sum)
