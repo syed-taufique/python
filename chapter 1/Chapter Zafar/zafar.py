d65c54b049914cece4bd089ee6754cd031bc1f4a
Taufique Ali <taufiqueali01@gmail.com>
2026-08-29 23:04:11 +0530

    Fix calculation to add two numbers instead of multiply
    Corrected the calculation from multiplication to addition for the second input pair.

--- chapter 1/Chapter Zafar/zafar.py
+++ chapter 1/Chapter Zafar/zafar.py
@@ -3,6 +3,7 @@ a =int(input("Enter a number: "))
 b=int(input("Enter another number: "))
 sum = a *b
 print("The product of", a, "and", b, "is:", sum)
+#addition of two numbers 
 a =int(input("Enter a number: "))
 b=int(input("Enter another number: "))
 sum = a +b
