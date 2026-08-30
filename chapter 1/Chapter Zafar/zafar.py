54991e09a2b64fb549a5ae27f38ed0f3910a70fb
Taufique Ali <taufiqueali01@gmail.com>
2026-08-31 00:06:51 +0530

    Fix spacing in sum calculation

--- chapter 1/Chapter Zafar/zafar.py
+++ chapter 1/Chapter Zafar/zafar.py
@@ -6,5 +6,5 @@ print("The product of", a, "and", b, "is:", sum)
 #addition of two numbers 
 a =int(input("Enter a number: "))
 b=int(input("Enter another number: "))
-sum = a +b
+sum = a + b
 print("The product of", a, "and", b, "is:", sum)
