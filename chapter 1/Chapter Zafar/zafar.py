78865f3c1054f5572a661c383f9a745332734351
Taufique Ali <taufiqueali01@gmail.com>
2026-09-02 23:15:40 +0530

    Fix formatting of multiplication operation

--- chapter 1/Chapter Zafar/zafar.py
+++ chapter 1/Chapter Zafar/zafar.py
@@ -1,7 +1,7 @@
 #sum of two numbers 
 a =int(input("Enter a number: "))
 b=int(input("Enter another number: "))
-sum = a *b
+sum = a * b
 print("The product of", a, "and", b, "is:", sum)
 #addition of two numbers 
 a =int(input("Enter a number: "))
